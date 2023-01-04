e28ff8b19ca288481660dcd5807fd65e8ee26aa4
Stuart Gibson <stuart@abovetheinternet.org>
2023-01-04 11:17:20 +0000

    Player data

---
+++ src/data/players.data.ts
@@ -0,0 +1,42 @@
+export const playersData = {
+  '80cfecae-ef2e-437b-bb94-f0309ee3b3d2': {
+    data: {
+      type: 'player',
+      id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+      attributes: {
+        displayName: 'Stuart',
+        userName: 'stuart'
+      }
+    }
+  },
+  'ff4976ce-1519-4ad6-a124-eef49565fc58': {
+    data: {
+      type: 'player',
+      id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+      attributes: {
+        displayName: 'Al McKinlay',
+        userName: 'Al'
+      }
+    }
+  },
+  'dba308cb-887e-4db2-913a-3a80caacd932': {
+    data: {
+      type: 'player',
+      id: 'dba308cb-887e-4db2-913a-3a80caacd932',
+      attributes: {
+        displayName: 'Nadia',
+        userName: 'Mudkip'
+      }
+    }
+  },
+  '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d': {
+    data: {
+      type: 'player',
+      id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+      attributes: {
+        displayName: 'Tone',
+        userName: 'Tone'
+      }
+    }
+  },
+}
