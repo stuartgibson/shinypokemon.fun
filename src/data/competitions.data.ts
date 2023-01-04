3ea86947fb9dcaf594b225e13a06d286b4403de3
Stuart Gibson <stuart@abovetheinternet.org>
2023-01-04 12:07:28 +0000

    Build

--- src/data/competitions.data.ts
+++ src/data/competitions.data.ts
@@ -1,5 +1,7 @@
 import { competitionsData2022 } from "./competitions/2022.data";
+import { competitionsData2023 } from "./competitions/2023.data";
 
 export const competitionsData = {
-  ...competitionsData2022
+  ...competitionsData2022,
+  ...competitionsData2023
 }
---
+++ src/data/competitions/2023.data.ts
@@ -0,0 +1,30 @@
+export const competitionsData2023 = {
+  'f0e61570-110a-4413-8c1f-f68beb482279': {
+    data: {
+      type: 'competition',
+      id: 'f0e61570-110a-4413-8c1f-f68beb482279',
+      attributes: {
+        description: '',
+        endDate: '2023-01-21',
+        startDate: '2023-01-08',
+        theme: 'TBA'
+      },
+      relationships: {
+        year: {
+          data: {
+            id: '04a6f41e-ab0d-4ad1-b909-f462d47778f7',
+            type: 'year'
+          }
+        },
+        validPokemon: [
+          // {
+          //   data: {
+          //     id: '1',
+          //     type: 'pokemon'
+          //   }
+          // }
+        ]
+      }
+    }
+  }
+}
