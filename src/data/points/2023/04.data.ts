e123c5721b00ab3815b355003273c48708f3e522
Stuart Gibson <stuart@abovetheinternet.org>
2023-02-24 12:05:13 +0000

    Deploy

--- src/data/points/2023/04.data.ts
+++ src/data/points/2023/04.data.ts
@@ -218,5 +218,221 @@ export const pointsData2023_4 = {
         }
       }
     }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Max (@Max)
+  //  0384. Rayquaza
+  'a544e5a6-f147-4e5c-907b-3b1efc4927bb': {
+    data: {
+      id: 'a544e5a6-f147-4e5c-907b-3b1efc4927bb',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-02-22',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRaid' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ff71afd8-290b-4522-b4c0-64e34e2bea71',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '0171898c-529c-4d3c-8be2-54c578cd54f9',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Bee Everfolly (@Bee Everfolly)
+  //  0427. Buneary
+  'db71a9e5-ad79-4a37-b36e-2ece9bb1c0de': {
+    data: {
+      id: 'db71a9e5-ad79-4a37-b36e-2ece9bb1c0de',
+      type: 'point',
+      attributes: {
+        ball: '—' as BallType,
+        catchDate: '2023-02-23',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '76a86523-df28-4bc2-b3a9-d6fab54ed027',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'a269af69-71bb-472d-87b7-195e46062ba3',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Tone (@Tone)
+  //  0198. Murkrow
+  '827751b1-ebd2-4d83-8e2f-ea03ccae5dbf': {
+    data: {
+      id: '827751b1-ebd2-4d83-8e2f-ea03ccae5dbf',
+      type: 'point',
+      attributes: {
+        ball: 'great' as BallType,
+        catchDate: '2023-02-23',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'b611b2fa-3178-4735-9891-52d9571058bf',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Lewis Dobie (@Lewthelegend)
+  //  0374. Beldum
+  '58bd0436-8046-4f87-ad32-6d1b5be6896e': {
+    data: {
+      id: '58bd0436-8046-4f87-ad32-6d1b5be6896e',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-02-23',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoEvent' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'efe1c7a0-9333-40cf-8017-6a04a9a4187d',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Robert Lewis (@A1966Mustang)
+  //  0374. Beldum
+  '77c30b79-9a4b-4d60-b1e8-2142a0176870': {
+    data: {
+      id: '77c30b79-9a4b-4d60-b1e8-2142a0176870',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-02-23',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '3657e524-63a2-4eb6-b768-c20b104e41a1',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'efe1c7a0-9333-40cf-8017-6a04a9a4187d',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Base Stats Over 500 19 Feb 2023  to 4 Mar 2023
+  //  Bee Everfolly (@Bee Everfolly)
+  //  0333. Swablu
+  '5ca6c457-85bd-4057-a110-fdf2166be769': {
+    data: {
+      id: '5ca6c457-85bd-4057-a110-fdf2166be769',
+      type: 'point',
+      attributes: {
+        ball: 'quick' as BallType,
+        catchDate: '2023-02-24',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'random' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '1cf610c6-c557-4ae5-9493-83a25187ecee',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '76a86523-df28-4bc2-b3a9-d6fab54ed027',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '9c6a7a1e-296d-447e-9293-713645a435bf',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
   }
 }
