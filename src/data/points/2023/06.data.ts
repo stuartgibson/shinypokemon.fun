bd6dc888973b93a13aed16ffdfeb6a45c96e35ab
Stuart Gibson <stuart@abovetheinternet.org>
2023-03-28 21:21:20 +0100

    Deploy

--- src/data/points/2023/06.data.ts
+++ src/data/points/2023/06.data.ts
@@ -75,5 +75,365 @@ export const pointsData2023_6:IPointEntities = {
         }
       }
     }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0088. Grimer
+  'ea2ca61b-b588-4989-80e9-f383bdd1400c': {
+    data: {
+      id: 'ea2ca61b-b588-4989-80e9-f383bdd1400c',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-20',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'f1339d6e-5bd4-4974-a746-2db83cfc5542',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0089. Muk
+  '7b0b3cba-1972-41e4-9b06-ece4e1a2bd7a': {
+    data: {
+      id: '7b0b3cba-1972-41e4-9b06-ece4e1a2bd7a',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-20',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '3bb72469-4db2-4e94-9dd7-7647fed36c0d',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0691. Dragalge
+  'aa4b472b-d821-4b11-925d-7cc2155fcdd2': {
+    data: {
+      id: 'aa4b472b-d821-4b11-925d-7cc2155fcdd2',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-20',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'ea3941f8-2fda-443e-b80c-d7ddd8e00d12',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0211. Qwilfish
+  '82695cd0-5441-477d-ae59-0a38400d4f14': {
+    data: {
+      id: '82695cd0-5441-477d-ae59-0a38400d4f14',
+      type: 'point',
+      attributes: {
+        ball: 'luxury' as BallType,
+        catchDate: '2023-03-20',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'd9865e99-9f5c-4598-b66e-bc0709208944',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0690. Skrelp
+  '7aa65bd5-699c-4cba-b0dc-3498b097c1fa': {
+    data: {
+      id: '7aa65bd5-699c-4cba-b0dc-3498b097c1fa',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-20',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '0b4905b3-bd2f-4714-bbdb-c88474d52b6f',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Robert Lewis (@A1966Mustang)
+  //  0970. Glimmora
+  '01b02c39-18ea-4e24-b936-4b988182d0e1': {
+    data: {
+      id: '01b02c39-18ea-4e24-b936-4b988182d0e1',
+      type: 'point',
+      attributes: {
+        ball: 'dive' as BallType,
+        catchDate: '2023-03-21',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '3657e524-63a2-4eb6-b768-c20b104e41a1',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '94e3ad92-d4e9-4e65-9165-b5b6d7eabfcd',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  james (@seginus)
+  //  0453. Croagunk
+  'e885a920-5ea0-4355-a6c9-c71e03bd7285': {
+    data: {
+      id: 'e885a920-5ea0-4355-a6c9-c71e03bd7285',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-22',
+        firstCatch: false,
+        game: 'violet' as GameType,
+        method: '—' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '8c294829-ea44-4cdc-9456-fed6cfe96151',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Devilish Inferno (@Alex)
+  //  0088. Grimer
+  'd60b45c3-4fe5-44a7-9af4-d94a2fd14e6d': {
+    data: {
+      id: 'd60b45c3-4fe5-44a7-9af4-d94a2fd14e6d',
+      type: 'point',
+      attributes: {
+        ball: 'pokeball' as BallType,
+        catchDate: '2023-03-23',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '3be16e09-d7c4-4ea5-a5bc-8fb3366355b6',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'f1339d6e-5bd4-4974-a746-2db83cfc5542',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Nama Chibitty (@NamaTheNerd)
+  //  0023. Ekans
+  '400cb460-f208-4d9d-b867-c99ebcbffc9e': {
+    data: {
+      id: '400cb460-f208-4d9d-b867-c99ebcbffc9e',
+      type: 'point',
+      attributes: {
+        ball: 'nest' as BallType,
+        catchDate: '2023-03-24',
+        firstCatch: false,
+        game: '—' as GameType,
+        method: '—' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'cfedba07-3cbe-4825-8c40-8623c2b89e31',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'c4908eda-0b33-4bbb-9501-e545a408c38c',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Poison 19 Mar 2023  to 1 Apr 2023
+  //  Al McKinlay (@Al)
+  //  0194. Wooper (Paldean)
+  'bb55bc6c-3ff6-4019-bb21-d26eebd632c2': {
+    data: {
+      id: 'bb55bc6c-3ff6-4019-bb21-d26eebd632c2',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-03-28',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: '—' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '4c08c4e6-8137-4761-b844-9c0c2d38ab2d',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '0af2156b-5af7-4e4d-a527-1c38e6781074',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
   }
 }
