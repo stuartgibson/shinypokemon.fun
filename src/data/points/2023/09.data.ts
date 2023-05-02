dc9897d1e227efc5ec372d7fc7823d076c683d6a
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2023-05-02 10:01:34 +0100

    Deploy

--- src/data/points/2023/09.data.ts
+++ src/data/points/2023/09.data.ts
@@ -1,5 +1,295 @@
 import { IPointEntities } from "store/reducers";
+import { BallType } from "types/ball.types";
+import { GameType } from "types/game.types";
+import { MethodType } from "types/method.types";
 
 export const pointsData2023_9:IPointEntities = {
-
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Padukes (@Padukes)
+  //  0283. Surskit
+  '7e053d11-a56e-4922-b12f-231dfb6e7e90': {
+    data: {
+      id: '7e053d11-a56e-4922-b12f-231dfb6e7e90',
+      type: 'point',
+      attributes: {
+        ball: 'dive' as BallType,
+        catchDate: '2023-04-30',
+        firstCatch: true,
+        game: 'scarlet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '81a4fe46-0079-418d-b3c4-882bca5a26e6',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Padukes (@Padukes)
+  //  0284. Masquerain
+  'f29d990c-1b43-444c-97fa-3d1944bdac35': {
+    data: {
+      id: 'f29d990c-1b43-444c-97fa-3d1944bdac35',
+      type: 'point',
+      attributes: {
+        ball: 'safari' as BallType,
+        catchDate: '2023-04-30',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '97f4cf7a-43de-47f9-a734-2b3b5c8870eb',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  james (@seginus)
+  //  0123. Scyther
+  '1df776f8-e038-43d3-a103-d21026c20d62': {
+    data: {
+      id: '1df776f8-e038-43d3-a103-d21026c20d62',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-04-30',
+        firstCatch: false,
+        game: 'violet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'f6e13a94-bcb0-4747-8c0e-cf45331cff80',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Tone (@Tone)
+  //  0636. Larvesta
+  '997c2b02-f6f3-48ed-8f86-81b46c448a6d': {
+    data: {
+      id: '997c2b02-f6f3-48ed-8f86-81b46c448a6d',
+      type: 'point',
+      attributes: {
+        ball: 'luxury' as BallType,
+        catchDate: '2023-04-30',
+        firstCatch: false,
+        game: 'violet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'f3f2667e-99f3-4b9f-ba31-61c03aceb378',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Tone (@Tone)
+  //  0637. Volcarona
+  '292755c9-fe0e-4719-9499-518a93ff7254': {
+    data: {
+      id: '292755c9-fe0e-4719-9499-518a93ff7254',
+      type: 'point',
+      attributes: {
+        ball: 'safari' as BallType,
+        catchDate: '2023-04-30',
+        firstCatch: false,
+        game: 'violet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '149043bb-0730-4320-bdc6-fa219e308223',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Al McKinlay (@Al)
+  //  0917. Tarountula
+  '0e107683-55c2-484a-8197-3f1c0a082bc0': {
+    data: {
+      id: '0e107683-55c2-484a-8197-3f1c0a082bc0',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-05-01',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'ff315a13-b4dc-4601-aca3-13a16fda7a22',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0415. Combee
+  'e853ea33-a3e5-4445-931a-a277acd987d7': {
+    data: {
+      id: 'e853ea33-a3e5-4445-931a-a277acd987d7',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-05-01',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '1d583683-c220-41fd-9175-281a26207a83',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  Bugs! 30 Apr 2023  to 13 May 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0401. Kricketot
+  '24655ae1-db15-482a-b8fa-ab363695d215': {
+    data: {
+      id: '24655ae1-db15-482a-b8fa-ab363695d215',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-05-01',
+        firstCatch: false,
+        game: 'scarlet' as GameType,
+        method: 'sandwich' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3f3707d8-d907-49d5-9ba8-7fd0b1a04617',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '507242bb-e2ae-4035-ad7e-c181a256fd6d',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  }
 }
