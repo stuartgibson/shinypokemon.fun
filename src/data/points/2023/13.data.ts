93f7a4eec8e1a904cb269145ec01d05da940abdc
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2023-06-27 15:47:38 +0100

    Deploy

--- src/data/points/2023/13.data.ts
+++ src/data/points/2023/13.data.ts
@@ -1,4 +1,5 @@
 import { IPointEntities } from "store/reducers";
+import { BallType } from "types/ball.types";
 import { GameType } from "types/game.types";
 import { MethodType } from "types/method.types";
 
@@ -38,5 +39,42 @@ export const pointsData2023_13:IPointEntities = {
         }
       }
     }
+  },
+    //  Café Remix 25 Jun 2023  to 8 Jul 2023
+  //  Lewis Dobie (@Lewthelegend)
+  //  0129. Magikarp
+  '7830a8b9-69b0-43e0-87ec-6a1cb3cc867f': {
+    data: {
+      id: '7830a8b9-69b0-43e0-87ec-6a1cb3cc867f',
+      type: 'point',
+      attributes: {
+        ball: 'pokeball' as BallType,
+        catchDate: '2023-06-27',
+        firstCatch: false,
+        game: 'alphaSapphire' as GameType,
+        method: 'chainFish' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '5618f11e-bde4-4812-889e-7f55ac96f0e9',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '494c864d-773a-4707-8aec-a26e87c8f3a6',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
   }
+
 }
