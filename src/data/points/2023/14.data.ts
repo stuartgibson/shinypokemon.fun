01ee23c4df663291ca88c923bc435ea3129e3824
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2023-07-20 09:03:50 +0100

    Deploy

--- src/data/points/2023/14.data.ts
+++ src/data/points/2023/14.data.ts
@@ -39,5 +39,149 @@ export const pointsData2023_14:IPointEntities = {
         }
       }
     }
+  },
+    //  No Hands or Feet 9 Jul 2023  to 22 Jul 2023
+  //  Padukes (@Padukes)
+  //  0023. Ekans
+  '591fe83c-e15f-4ebb-b8d6-4011808ecdd1': {
+    data: {
+      id: '591fe83c-e15f-4ebb-b8d6-4011808ecdd1',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-07-09',
+        firstCatch: false,
+        game: null,
+        method: 'chain' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9c7535c6-7bf5-4c7b-b687-53b4e81f92dd',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'c4908eda-0b33-4bbb-9501-e545a408c38c',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  No Hands or Feet 9 Jul 2023  to 22 Jul 2023
+  //  Lewis Dobie (@Lewthelegend)
+  //  0422. Shellos
+  'e4a3d693-fab2-4dd6-924d-4cfd2a2019d5': {
+    data: {
+      id: 'e4a3d693-fab2-4dd6-924d-4cfd2a2019d5',
+      type: 'point',
+      attributes: {
+        ball: 'premier' as BallType,
+        catchDate: '2023-07-13',
+        firstCatch: false,
+        game: 'legendsArceus' as GameType,
+        method: 'outbreakLegendsArceus' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9c7535c6-7bf5-4c7b-b687-53b4e81f92dd',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '0091eadb-706c-40d1-ba11-5c95b383b46b',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  No Hands or Feet 9 Jul 2023  to 22 Jul 2023
+  //  Kathryn (@SteeleTheWin)
+  //  0095. Onix
+  'e1d5e1b4-fa38-4a85-9ea7-699b34b30846': {
+    data: {
+      id: 'e1d5e1b4-fa38-4a85-9ea7-699b34b30846',
+      type: 'point',
+      attributes: {
+        ball: 'pokeball' as BallType,
+        catchDate: '2023-07-14',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9c7535c6-7bf5-4c7b-b687-53b4e81f92dd',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'ec689f25-fffe-4249-b89d-603d574bacee',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'e6b8fffd-c432-4df7-85ba-4b8f88593ba7',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  },
+  //  No Hands or Feet 9 Jul 2023  to 22 Jul 2023
+  //  james (@seginus)
+  //  0204. Pineco
+  '06936957-f3e1-48d3-97c3-0f2b626b389c': {
+    data: {
+      id: '06936957-f3e1-48d3-97c3-0f2b626b389c',
+      type: 'point',
+      attributes: {
+        ball: '—' as BallType,
+        catchDate: '2023-07-15',
+        firstCatch: false,
+        game: 'pogo' as GameType,
+        method: 'pogoRandom' as MethodType
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9c7535c6-7bf5-4c7b-b687-53b4e81f92dd',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: 'da602af9-4a1d-40bf-9042-f943f7241a9d',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
   }
 }
