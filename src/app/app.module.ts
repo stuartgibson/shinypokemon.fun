236121c506c3793d3c3bb25831ffc84019ed02d0
Stuart Gibson <stuart@abovetheinternet.org>
2023-11-10 20:13:09 +0000

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,8 +42,8 @@ import { AppComponent } from './components/app/app.component';
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-    }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    , connectInZone: true}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() , connectInZone: true}),
     NgbModule,
   ],
   providers: [
