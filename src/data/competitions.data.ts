061a279fdff540a023602e4566f7da1c6f465f08
Stuart Gibson <stuart@abovetheinternet.org>
2024-01-07 17:45:19 +0000

    Deploy

--- src/data/competitions.data.ts
+++ src/data/competitions.data.ts
@@ -1,8 +1,10 @@
 import { ICompetitionEntity } from "store/reducers/competitions.reducer";
 import { competitionsData2022 } from "./competitions/2022.data";
 import { competitionsData2023 } from "./competitions/2023.data";
+import { competitionsData2024 } from "./competitions/2024.data";
 
 export const competitionsData:{[id:string]:ICompetitionEntity} = {
   ...competitionsData2022,
-  ...competitionsData2023
+  ...competitionsData2023,
+  ...competitionsData2024,
 }
