94bd985bc189a2eaef17ee51d0358211deb0b7d5
Stuart Gibson <stuart@abovetheinternet.org>
2024-01-08 17:50:23 +0000

    Deploy

--- src/data/points/2024/01.data.ts
+++ src/data/points/2024/01.data.ts
@@ -1,5 +1,78 @@
 import { IPointEntities } from "store/reducers";
 
 export const pointsData2024_1:IPointEntities = {
+  //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
+  //  Padukes (@Padukes)
+  //  0240. Magby
+  '3c37fdfa-8e0f-4ac9-8032-b7a66945071d': {
+    data: {
+      id: '3c37fdfa-8e0f-4ac9-8032-b7a66945071d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-07',
+        firstCatch: false,
+        game: null,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '745e1717-530f-423f-9d98-96457a1a218b',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  }
+,
 
+  //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
+  //  Padukes (@Padukes)
+  //  0126. Magmar
+  'cd2eb7fa-003c-4c85-abe8-21f2789aa95e': {
+    data: {
+      id: 'cd2eb7fa-003c-4c85-abe8-21f2789aa95e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-07',
+        firstCatch: false,
+        game: null,
+        method: null
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
+            type: 'competition'
+          }
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player'
+          }
+        },
+        pokemon: {
+          data: {
+            id: '9356e54c-96fb-4b1e-bd29-eb772f8056ac',
+            type: 'pokemon'
+          }
+        }
+      }
+    }
+  }
 }
