9cd57d54171575bc0bd9fb5ebac4f4d848225cd3
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-01-09 12:05:30 +0000

    Deploy

--- src/data/points/2024/01.data.ts
+++ src/data/points/2024/01.data.ts
@@ -1,6 +1,6 @@
-import { IPointEntities } from "store/reducers";
+import { IPointEntities } from 'store/reducers';
 
-export const pointsData2024_1:IPointEntities = {
+export const pointsData2024_1: IPointEntities = {
   //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
   //  Padukes (@Padukes)
   //  0240. Magby
@@ -13,32 +13,30 @@ export const pointsData2024_1:IPointEntities = {
         catchDate: '2024-01-07',
         firstCatch: true,
         game: null,
-        method: null
+        method: null,
       },
       relationships: {
         competition: {
           data: {
             id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
-            type: 'competition'
-          }
+            type: 'competition',
+          },
         },
         player: {
           data: {
             id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
-            type: 'player'
-          }
+            type: 'player',
+          },
         },
         pokemon: {
           data: {
             id: '745e1717-530f-423f-9d98-96457a1a218b',
-            type: 'pokemon'
-          }
-        }
-      }
-    }
-  }
-,
-
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
   //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
   //  Padukes (@Padukes)
   //  0126. Magmar
@@ -51,28 +49,100 @@ export const pointsData2024_1:IPointEntities = {
         catchDate: '2024-01-07',
         firstCatch: false,
         game: null,
-        method: null
+        method: null,
       },
       relationships: {
         competition: {
           data: {
             id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
-            type: 'competition'
-          }
+            type: 'competition',
+          },
         },
         player: {
           data: {
             id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
-            type: 'player'
-          }
+            type: 'player',
+          },
         },
         pokemon: {
           data: {
             id: '9356e54c-96fb-4b1e-bd29-eb772f8056ac',
-            type: 'pokemon'
-          }
-        }
-      }
-    }
-  }
-}
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
+  //  Al McKinlay (@Al)
+  //  0884. Duraludon
+  '37260c5a-6da7-4624-a92b-008dc727bc98': {
+    data: {
+      id: '37260c5a-6da7-4624-a92b-008dc727bc98',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ece1a260-dacb-4e7f-84a0-6f0b92c6fcbf',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Cross-Generational Evolution Families 7 Jan 2024  to 20 Jan 2024
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0137. Porygon
+  '15b43101-eb0c-46f9-ba62-22ccf29f6cd4': {
+    data: {
+      id: '15b43101-eb0c-46f9-ba62-22ccf29f6cd4',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'ee547f89-c68f-410e-b288-6f2c8db40e83',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '32db34f7-0c11-401c-8504-d399aa98e376',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
