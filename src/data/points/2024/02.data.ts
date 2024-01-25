0ba61c59df674e97d14855c632842ce7ed727807
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-01-25 10:42:36 +0000

    Deploy

--- src/data/points/2024/02.data.ts
+++ src/data/points/2024/02.data.ts
@@ -223,4 +223,374 @@ export const pointsData2024_2: IPointEntities = {
       },
     },
   },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Tone (@Tone)
+  //  0580. Ducklett
+  'f5f9edbc-cf0e-4473-a1aa-f1b62b409c69': {
+    data: {
+      id: 'f5f9edbc-cf0e-4473-a1aa-f1b62b409c69',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '583cf1b3-ece5-4a89-b07c-24416a748d1c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Al McKinlay (@Al)
+  //  0991. Iron Bundle
+  '3444e42e-6c42-4f26-bc55-996c3bb69ad9': {
+    data: {
+      id: '3444e42e-6c42-4f26-bc55-996c3bb69ad9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd067ea89-4cef-4a4e-9331-2d031120a807',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Devilish Inferno (@Alex)
+  //  0816. Sobble
+  '2eb6ffea-06dd-466a-967d-6f6e7bce8041': {
+    data: {
+      id: '2eb6ffea-06dd-466a-967d-6f6e7bce8041',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '3be16e09-d7c4-4ea5-a5bc-8fb3366355b6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a829996d-61ac-4a55-beac-8197735e7687',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Padukes (@Padukes)
+  //  0258. Mudkip
+  '9eda8157-27f2-4fff-9ff1-9b215f0d29b2': {
+    data: {
+      id: '9eda8157-27f2-4fff-9ff1-9b215f0d29b2',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a2a015a4-24b5-4ad2-87be-ce8817a8329e',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Padukes (@Padukes)
+  //  0258. Mudkip
+  '9f5d8a54-78b8-488c-95cd-296cd63fb48d': {
+    data: {
+      id: '9f5d8a54-78b8-488c-95cd-296cd63fb48d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a2a015a4-24b5-4ad2-87be-ce8817a8329e',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Níck Mārsh (@Citizen)
+  //  0054. Psyduck
+  'ae37599f-6b39-46b3-8b17-ae8ef95e7fb0': {
+    data: {
+      id: 'ae37599f-6b39-46b3-8b17-ae8ef95e7fb0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-22',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '61199295-67ee-46c8-baf8-42b51f83860a',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '0fee26f0-8367-4d19-a22c-dd1bbf6dc400',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Padukes (@Padukes)
+  //  0767. Wimpod
+  '95dd68c1-9fd3-4b59-b955-4fa4e11d1471': {
+    data: {
+      id: '95dd68c1-9fd3-4b59-b955-4fa4e11d1471',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '794f527e-ee32-4f4d-8b67-12fd54ec7a7b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Bobby De Walt (@Bobby)
+  //  0833. Chewtle
+  '800a621e-7c01-445b-9ca5-ab6101cec8fd': {
+    data: {
+      id: '800a621e-7c01-445b-9ca5-ab6101cec8fd',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-25',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'a3abfccd-9cb0-422a-b3f3-aceb26f8b678',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'dbed930c-3262-4fff-afca-dd52d4a02bd6',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Keagan (@Keagan)
+  //  0270. Lotad
+  '8f77a310-9992-4621-b4c4-b86f6e7dc465': {
+    data: {
+      id: '8f77a310-9992-4621-b4c4-b86f6e7dc465',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7f1a5f65-a490-4ef6-9add-b4774f56bc84',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Water Pokémon With Feet 21 Jan 2024  to 3 Feb 2024
+  //  Keagan (@Keagan)
+  //  0270. Lotad
+  'd3b9db7d-cfdd-4227-87e7-01344c32705e': {
+    data: {
+      id: 'd3b9db7d-cfdd-4227-87e7-01344c32705e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-01-21',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3be27f11-188f-413e-a064-cc31a75d9e19',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7f1a5f65-a490-4ef6-9add-b4774f56bc84',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
