67932efeab4993f4a2d1e283e91b457e6b6f19b5
Stuart Gibson <stuart@abovetheinternet.org>
2024-08-04 22:20:25 +0100

    Deploy

--- src/data/points/2024/16.data.ts
+++ src/data/points/2024/16.data.ts
@@ -1,3 +1,300 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2024_16: IPointEntities = {};
+export const pointsData2024_16: IPointEntities = {
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0968. Orthworm
+  '5ad93afa-c4cf-49cf-a0cf-2771a2cc94fd': {
+    data: {
+      id: '5ad93afa-c4cf-49cf-a0cf-2771a2cc94fd',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '4fff0c25-e12a-4d29-8880-a76dc2b1cc77',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Al McKinlay (@Al)
+  //  0987. Flutter Mane
+  '067fd2ec-6f7a-4dae-9fec-e18585f4e897': {
+    data: {
+      id: '067fd2ec-6f7a-4dae-9fec-e18585f4e897',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a711e10f-5109-43b8-8991-b826dcf1ce58',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0936. Armarouge
+  '603e5eee-5069-48ee-8f69-66b4e2b0ee9f': {
+    data: {
+      id: '603e5eee-5069-48ee-8f69-66b4e2b0ee9f',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd54d9dc3-e2be-4e94-92fe-ef99ccfcc073',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0937. Ceruledge
+  '9acd3811-48d8-478b-897c-5d674533482a': {
+    data: {
+      id: '9acd3811-48d8-478b-897c-5d674533482a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'adf10d91-040a-4dad-87c1-d8e66259ad30',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0970. Glimmora
+  '00eba462-4951-4af9-a777-9a3b0c18abfa': {
+    data: {
+      id: '00eba462-4951-4af9-a777-9a3b0c18abfa',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '94e3ad92-d4e9-4e65-9165-b5b6d7eabfcd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0059. Arcanine
+  'f61065a9-482f-4a81-ba9d-677121c38da3': {
+    data: {
+      id: 'f61065a9-482f-4a81-ba9d-677121c38da3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'fd68bfbd-da6e-4df4-9d00-e549d1638fd3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0184. Azumarill
+  '528a040e-6844-4420-9197-e74a7081b030': {
+    data: {
+      id: '528a040e-6844-4420-9197-e74a7081b030',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '40302532-8dc3-4a8b-9669-5c6f19ae7290',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Stuart (@stuart)
+  //  0475. Gallade
+  'a801b33c-352f-4d2d-88aa-0fb947113fea': {
+    data: {
+      id: 'a801b33c-352f-4d2d-88aa-0fb947113fea',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '34f1dd7b-5470-4203-9abe-93ce7bc49442',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
