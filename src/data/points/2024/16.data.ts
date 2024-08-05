c6b7f467f2ea5ca3b4ea1902a3a1108e425ca799
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-08-05 09:25:17 +0100

    Deploy

--- src/data/points/2024/16.data.ts
+++ src/data/points/2024/16.data.ts
@@ -297,4 +297,263 @@ export const pointsData2024_16: IPointEntities = {
       },
     },
   },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Padukes (@Padukes)
+  //  0212. Scizor
+  '586e6ea9-56d2-4a03-8200-01a693560f3c': {
+    data: {
+      id: '586e6ea9-56d2-4a03-8200-01a693560f3c',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '0874e9e6-f4a0-4114-83c1-fabc0dfa665c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Padukes (@Padukes)
+  //  0459. Snover
+  '078eb037-9f21-4eeb-b3b8-88ab3a0889c7': {
+    data: {
+      id: '078eb037-9f21-4eeb-b3b8-88ab3a0889c7',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'fd2affc4-e41b-418e-a559-62741f95da21',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Padukes (@Padukes)
+  //  0198. Murkrow
+  '2038636e-1429-4fe5-9726-f75c373092d6': {
+    data: {
+      id: '2038636e-1429-4fe5-9726-f75c373092d6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b611b2fa-3178-4735-9891-52d9571058bf',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Padukes (@Padukes)
+  //  0548. Petilil
+  '5fd2f60d-7dbe-4901-8cfc-fa895f22a02d': {
+    data: {
+      id: '5fd2f60d-7dbe-4901-8cfc-fa895f22a02d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '34294645-8d12-4314-bbf7-ff68ed5bc199',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Níck Mārsh (@Citizen)
+  //  0991. Iron Bundle
+  'e490bb02-1172-44f7-ab9e-4dce24be7836': {
+    data: {
+      id: 'e490bb02-1172-44f7-ab9e-4dce24be7836',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '61199295-67ee-46c8-baf8-42b51f83860a',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd067ea89-4cef-4a4e-9331-2d031120a807',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Níck Mārsh (@Citizen)
+  //  0183. Marill
+  'ba2268fd-16a0-4a18-965c-3b739f01dcb4': {
+    data: {
+      id: 'ba2268fd-16a0-4a18-965c-3b739f01dcb4',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '61199295-67ee-46c8-baf8-42b51f83860a',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ed377739-ba16-43b7-9292-399ba1dd7d29',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World's Top 8 4 Aug 2024  to 17 Aug 2024
+  //  Níck Mārsh (@Citizen)
+  //  0130. Gyarados
+  'd35e8dd2-4ce6-4cf9-8f8a-4536230232a9': {
+    data: {
+      id: 'd35e8dd2-4ce6-4cf9-8f8a-4536230232a9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-08-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '537a93cd-19f4-4fdc-829a-185a96c5982f',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '61199295-67ee-46c8-baf8-42b51f83860a',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9ee2c16a-577f-4cc8-8839-1a4116c7480b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
