8dc679b283aade5550e5d3bf736664eb1a557766
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-08-06 13:16:03 +0100

    Deploy

--- src/app/components/competitions/competition-page/competition-page.component.ts
+++ src/app/components/competitions/competition-page/competition-page.component.ts
@@ -1,32 +1,43 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  Signal,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { CompetitionActions } from 'store/actions/competition.actions';
-import { competitionPageViewModel, CompetitionPageViewModel } from 'store/view-models/competition-page.view-model';
-import { PokemonComponent } from '../../pokemon/pokemon/pokemon.component';
+import {
+  competitionPageViewModel,
+  CompetitionPageViewModel,
+} from 'store/view-models/competition-page.view-model';
 import { PointComponent } from '../../point/point.component';
+import { PokemonComponent } from '../../pokemon/pokemon/pokemon.component';
 import { CompetitionComponent } from '../competition/competition.component';
 
 @Component({
-    selector: 'sp-competition-page',
-    templateUrl: './competition-page.component.html',
-    styleUrls: ['./competition-page.component.sass'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [CompetitionComponent, PointComponent, PokemonComponent]
+  selector: 'sp-competition-page',
+  templateUrl: './competition-page.component.html',
+  styleUrls: ['./competition-page.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CompetitionComponent, PointComponent, PokemonComponent, RouterLink],
 })
 export class CompetitionPageComponent implements OnDestroy {
+  private readonly store: Store = inject(Store);
 
-  private readonly store:Store = inject(Store);
+  vm: Signal<CompetitionPageViewModel> = this.store.selectSignal(
+    competitionPageViewModel
+  );
 
-  vm:Signal<CompetitionPageViewModel> = this.store.selectSignal(competitionPageViewModel);
-
-  filterPokemon(event:any):void {
+  filterPokemon(event: any): void {
     this.store.dispatch(
-      CompetitionActions.filter({query: event.target.value})
+      CompetitionActions.filter({ query: event.target.value })
     );
   }
 
-  ngOnDestroy():void {
+  ngOnDestroy(): void {
     this.store.dispatch(CompetitionActions.clear());
   }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,57 +1,61 @@
-import { Routes } from "@angular/router";
-import { GeneratorsComponent } from "./components/admin/generators/generators.component";
-import { CompetitionPageComponent } from "./components/competitions/competition-page/competition-page.component";
-import { CompetitionsComponent } from "./components/competitions/competitions/competitions.component";
-import { HomeComponent } from "./components/home/home.component";
-import { AboutComponent } from "./components/pages/about/about.component";
-import { RulesComponent } from "./components/pages/rules/rules.component";
-import { PlayerPageComponent } from "./components/player/player-page/player-page.component";
-import { PlayersComponent } from "./components/player/players/players.component";
-import { YearPageComponent } from "./components/years/year-page/year-page.component";
-import { CurrentCompetitionGuard } from "./current-competition-guard";
+import { Routes } from '@angular/router';
+import { GeneratorsComponent } from './components/admin/generators/generators.component';
+import { CompetitionPageInfographicComponent } from './components/competitions/competition-page/competition-page-infographic.component';
+import { CompetitionPageComponent } from './components/competitions/competition-page/competition-page.component';
+import { CompetitionsComponent } from './components/competitions/competitions/competitions.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { RulesComponent } from './components/pages/rules/rules.component';
+import { PlayerPageComponent } from './components/player/player-page/player-page.component';
+import { PlayersComponent } from './components/player/players/players.component';
+import { YearPageComponent } from './components/years/year-page/year-page.component';
+import { CurrentCompetitionGuard } from './current-competition-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'about',
-    component: AboutComponent
+    component: AboutComponent,
   },
   {
     path: 'competitions',
-    component: CompetitionsComponent
+    component: CompetitionsComponent,
   },
   {
     path: 'competitions/current',
     canActivate: [CurrentCompetitionGuard],
-    component: CompetitionsComponent
+    component: CompetitionsComponent,
   },
   {
     path: 'competitions/:id',
-    component: CompetitionPageComponent
+    component: CompetitionPageComponent,
+  },
+  {
+    path: 'competitions/:id/infographic',
+    component: CompetitionPageInfographicComponent,
   },
   {
     path: 'admin/generators',
-    component: GeneratorsComponent
+    component: GeneratorsComponent,
   },
   {
     path: 'players',
-    component: PlayersComponent
+    component: PlayersComponent,
   },
   {
     path: 'players/:id',
-    component: PlayerPageComponent
+    component: PlayerPageComponent,
   },
   {
     path: 'rules',
-    component: RulesComponent
+    component: RulesComponent,
   },
   {
     path: 'years/:id',
-    component: YearPageComponent
-  }
+    component: YearPageComponent,
+  },
 ];
-
