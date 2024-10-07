afc45be8a5347f2bfa86852d08ef3a3a1943a15f
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-10-07 12:25:17 +0100

    Deploy

--- src/data/points/2024/20.data.ts
+++ src/data/points/2024/20.data.ts
@@ -149,4 +149,189 @@ export const pointsData2024_20: IPointEntities = {
       },
     },
   },
+  //  Paldea 29 Sep 2024  to 12 Oct 2024
+  //  james (@seginus)
+  //  0917. Tarountula
+  '233af0d2-0c37-4c1b-a4f7-a8ecfa242400': {
+    data: {
+      id: '233af0d2-0c37-4c1b-a4f7-a8ecfa242400',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-10-01',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b9d07633-4984-440f-93c8-746455e504bb',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ff315a13-b4dc-4601-aca3-13a16fda7a22',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Paldea 29 Sep 2024  to 12 Oct 2024
+  //  Padukes (@Padukes)
+  //  0206. Dunsparce
+  '244d3c27-5e29-4a73-93c0-9370bc84fc2e': {
+    data: {
+      id: '244d3c27-5e29-4a73-93c0-9370bc84fc2e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-10-02',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b9d07633-4984-440f-93c8-746455e504bb',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '41115700-66dd-40bf-a5bf-a8b1949b537b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Paldea 29 Sep 2024  to 12 Oct 2024
+  //  Stuart (@stuart)
+  //  0840. Applin
+  '3d8f5857-ec8c-440d-94da-32d001476cb5': {
+    data: {
+      id: '3d8f5857-ec8c-440d-94da-32d001476cb5',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-10-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b9d07633-4984-440f-93c8-746455e504bb',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '568746c3-51f1-4050-bbe7-66829f09e3df',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Paldea 29 Sep 2024  to 12 Oct 2024
+  //  Stuart (@stuart)
+  //  0840. Applin
+  '3d4d5585-7af0-4503-b98a-894f7fe90cd6': {
+    data: {
+      id: '3d4d5585-7af0-4503-b98a-894f7fe90cd6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-10-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b9d07633-4984-440f-93c8-746455e504bb',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '568746c3-51f1-4050-bbe7-66829f09e3df',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Paldea 29 Sep 2024  to 12 Oct 2024
+  //  Devilish Inferno (@Alex)
+  //  1012. Poltchageist
+  '964af839-37e2-4914-b266-8a03f1f02269': {
+    data: {
+      id: '964af839-37e2-4914-b266-8a03f1f02269',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-10-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b9d07633-4984-440f-93c8-746455e504bb',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '3be16e09-d7c4-4ea5-a5bc-8fb3366355b6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '1e1957a8-49bd-49e6-ab63-0ce298dabaca',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
