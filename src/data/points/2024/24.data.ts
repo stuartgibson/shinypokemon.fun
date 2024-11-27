5db8bd2893e7c854cc9ad3835325468f309f024e
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2024-11-27 10:48:50 +0000

    Deploy

--- src/data/points/2024/24.data.ts
+++ src/data/points/2024/24.data.ts
@@ -38,4 +38,707 @@ export const pointsData2024_24: IPointEntities = {
       },
     },
   },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Al McKinlay (@Al)
+  //  0211. Qwilfish
+  '1d04fdb7-83b3-47d7-9450-13bf5460fbe7': {
+    data: {
+      id: '1d04fdb7-83b3-47d7-9450-13bf5460fbe7',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd9865e99-9f5c-4598-b66e-bc0709208944',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  james (@seginus)
+  //  0473. Mamoswine
+  '3273103a-69ab-435e-8395-2c9019cae39a': {
+    data: {
+      id: '3273103a-69ab-435e-8395-2c9019cae39a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '0a7c7eba-cc15-4542-8394-f1646442ebe1',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  james (@seginus)
+  //  0382. Kyogre
+  'a30062d4-2e49-44ad-86aa-91409d53792d': {
+    data: {
+      id: 'a30062d4-2e49-44ad-86aa-91409d53792d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'c159b074-9e9f-4e1b-889f-a64dd78598a6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3bb1a893-8647-4227-8624-874b958f7e74',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Jette (@Jette)
+  //  0473. Mamoswine
+  'eb51341b-b31d-46fb-b31d-e62d40fb1b9e': {
+    data: {
+      id: 'eb51341b-b31d-46fb-b31d-e62d40fb1b9e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'a84bf904-f147-4e6e-9053-245d8c95bb7b',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '0a7c7eba-cc15-4542-8394-f1646442ebe1',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Tone (@Tone)
+  //  0747. Mareanie
+  '6fa6f0a8-7cf1-4cb3-9841-17bbe4db7f11': {
+    data: {
+      id: '6fa6f0a8-7cf1-4cb3-9841-17bbe4db7f11',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '77f390a4-0d38-434e-af28-8c858e060b3c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Tone (@Tone)
+  //  0211. Qwilfish (Hisuian)
+  'd207524c-d9fc-479c-b293-68951b07c3ee': {
+    data: {
+      id: 'd207524c-d9fc-479c-b293-68951b07c3ee',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '97308d9e-46b4-42f5-8c19-f4ea556d4c9f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0484. Palkia
+  '1276d81d-665c-4794-bd4f-044f8f07e881': {
+    data: {
+      id: '1276d81d-665c-4794-bd4f-044f8f07e881',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '2cba6507-318a-4559-ab8d-b28f19303ee6',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0483. Dialga
+  'ab710a68-fdc9-48ab-9769-928f98b49e0a': {
+    data: {
+      id: 'ab710a68-fdc9-48ab-9769-928f98b49e0a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ae79d2b8-50fa-4ddb-ac8b-42c2563ba265',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Padukes (@Padukes)
+  //  0550. Basculin
+  '0de4f60d-e581-425a-8f17-e08702daab21': {
+    data: {
+      id: '0de4f60d-e581-425a-8f17-e08702daab21',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7411e93e-127f-42ee-99fe-d44f31e7b60a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0211. Qwilfish
+  '4e7a38c1-ed6a-4c69-a439-52ec7191692a': {
+    data: {
+      id: '4e7a38c1-ed6a-4c69-a439-52ec7191692a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd9865e99-9f5c-4598-b66e-bc0709208944',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Padukes (@Padukes)
+  //  0220. Swinub
+  'c065b514-02a8-4618-a071-38026a8c55eb': {
+    data: {
+      id: 'c065b514-02a8-4618-a071-38026a8c55eb',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9dfb92df-d030-412c-9833-d5406c3051e3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0614. Beartic
+  '32983637-126d-456d-8483-db29faed7775': {
+    data: {
+      id: '32983637-126d-456d-8483-db29faed7775',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a1bb598f-7991-4bfe-b0c1-da273b4304bb',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0131. Lapras
+  '836c0ed5-fa78-46b1-be3c-ff17e108b7aa': {
+    data: {
+      id: '836c0ed5-fa78-46b1-be3c-ff17e108b7aa',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'eaf401e1-ddb3-4917-bda3-f7a6b3046ba3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0027. Sandshrew (Alolan)
+  '564294f1-d97c-4ad2-88f8-ab2fe4ad448a': {
+    data: {
+      id: '564294f1-d97c-4ad2-88f8-ab2fe4ad448a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '1dc0b2ae-31d4-4759-9908-28efae57fc79',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Padukes (@Padukes)
+  //  0457. Lumineon
+  'c64fa1f7-65f3-4c76-8496-139a9e37d8b3': {
+    data: {
+      id: 'c64fa1f7-65f3-4c76-8496-139a9e37d8b3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-25',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '05986577-0d49-4e49-9556-4a7c1d681794',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Stuart (@stuart)
+  //  0158. Totodile
+  '77eb766a-0394-4464-a18b-62ba830d2e56': {
+    data: {
+      id: '77eb766a-0394-4464-a18b-62ba830d2e56',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '92d5db27-8ee7-4831-8924-62e9ccbd69a4',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Stuart (@stuart)
+  //  0779. Bruxish
+  'eb0c7561-19aa-4a5c-8ab7-c93e6ff7d420': {
+    data: {
+      id: 'eb0c7561-19aa-4a5c-8ab7-c93e6ff7d420',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'd06db98e-bac0-4676-bf64-0cb0101ef847',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Keagan (@Keagan)
+  //  0656. Froakie
+  'e8123224-fd1c-46e1-bcbf-dd558dc6992a': {
+    data: {
+      id: 'e8123224-fd1c-46e1-bcbf-dd558dc6992a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9086e1d6-b646-466d-afad-5dcdbb61654e',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon that learn the move Blizzard 24 Nov 2024  to 7 Dec 2024
+  //  Níck Mārsh (@Citizen)
+  //  0483. Dialga
+  '84ebc85d-c66c-4fc2-8eea-cfcdc99a09d1': {
+    data: {
+      id: '84ebc85d-c66c-4fc2-8eea-cfcdc99a09d1',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2024-11-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '9cb21b45-d302-49ef-b2dd-507be2af43ec',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '61199295-67ee-46c8-baf8-42b51f83860a',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ae79d2b8-50fa-4ddb-ac8b-42c2563ba265',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
