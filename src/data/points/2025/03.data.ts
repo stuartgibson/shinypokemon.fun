f1f8917c582a0c9a9be4bcd522b0028298ad0317
Stuart Gibson <stuart@abovetheinternet.org>
2025-02-16 09:39:38 +0000

    Deploy

--- src/data/points/2025/03.data.ts
+++ src/data/points/2025/03.data.ts
@@ -260,4 +260,448 @@ export const pointsData2025_03: IPointEntities = {
       },
     },
   },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Al McKinlay (@Al)
+  //  0996. Frigibax
+  '874cd3c5-0a0d-4ca5-b0aa-2576fcf4747d': {
+    data: {
+      id: '874cd3c5-0a0d-4ca5-b0aa-2576fcf4747d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9f8c7a79-1fcc-4c5c-95b5-a6e49d170c5b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Lewis Dobie (@Lewthelegend)
+  //  0100. Voltorb
+  '6fba7a95-905e-4d65-8b63-19669d399b1e': {
+    data: {
+      id: '6fba7a95-905e-4d65-8b63-19669d399b1e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a7ea779c-7360-4fae-a051-3db40f8cbf45',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Bobby De Walt (@Bobby)
+  //  0613. Cubchoo
+  '45557df9-115a-40c5-a39e-02d72abe667d': {
+    data: {
+      id: '45557df9-115a-40c5-a39e-02d72abe667d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'a3abfccd-9cb0-422a-b3f3-aceb26f8b678',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9219d4f4-2bd8-4568-a1d0-1f4f12fe6cbd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Tone (@Tone)
+  //  0602. Tynamo
+  'b22bb9b4-9233-4852-916a-6bfbe50e9970': {
+    data: {
+      id: 'b22bb9b4-9233-4852-916a-6bfbe50e9970',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-06',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7e20ee70-a9eb-4d71-87ff-796131965f57',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Padukes (@Padukes)
+  //  0555. Darmanitan
+  '0ae1f230-3332-4160-975b-f2b7f55a3595': {
+    data: {
+      id: '0ae1f230-3332-4160-975b-f2b7f55a3595',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-06',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7b16a8cc-41ec-41d5-b04e-69e77765777e',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0096. Drowzee
+  '91cfb729-4a33-46f9-a1e1-778a5096a9c3': {
+    data: {
+      id: '91cfb729-4a33-46f9-a1e1-778a5096a9c3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-07',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'e3d7e9a1-85cf-471c-86ec-18bf93ecefd2',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Keagan (@Keagan)
+  //  0202. Wobbuffet
+  'b4012d07-f193-4e19-95b5-981245d0c91c': {
+    data: {
+      id: 'b4012d07-f193-4e19-95b5-981245d0c91c',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7c8faea8-5ba1-4c3c-9867-f8f3d9defcd7',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Keagan (@Keagan)
+  //  0836. Boltund
+  'd41f3364-960f-4181-902f-76199b17fce6': {
+    data: {
+      id: 'd41f3364-960f-4181-902f-76199b17fce6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '45ac1a90-8a7e-44dd-8d2c-d8c5e30baaa9',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Stuart (@stuart)
+  //  0574. Gothita
+  'f0a35b3a-5bab-40ff-8dba-ed57fecbdd9c': {
+    data: {
+      id: 'f0a35b3a-5bab-40ff-8dba-ed57fecbdd9c',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '571e8806-a993-449a-9d8e-11ce979ca911',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Padukes (@Padukes)
+  //  0201. Unown
+  'fe0601e4-4306-4abb-a19c-bd98e65519e3': {
+    data: {
+      id: 'fe0601e4-4306-4abb-a19c-bd98e65519e3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'c47787a9-7b74-4f4b-93e9-e0836ef7cd28',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Padukes (@Padukes)
+  //  0201. Unown
+  'b841075b-0f58-4532-93a5-65a889c98171': {
+    data: {
+      id: 'b841075b-0f58-4532-93a5-65a889c98171',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-11',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'c47787a9-7b74-4f4b-93e9-e0836ef7cd28',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Missing Legendaries 2 Feb 2025  to 15 Feb 2025
+  //  Padukes (@Padukes)
+  //  0713. Avalugg
+  '4dd4ecf4-c021-4c27-beff-de919d61cf9d': {
+    data: {
+      id: '4dd4ecf4-c021-4c27-beff-de919d61cf9d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-02-13',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'c0dcbd35-c437-4747-afd9-391d1cd3eb1c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '06e87530-bdd5-41cd-8a70-197d8f70035a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
