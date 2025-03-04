b2fe4c04d031fc9dadf0c359fb42d4b61eec78c5
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2025-03-04 12:20:28 +0000

    Deploy

--- src/data/points/2025/05.data.ts
+++ src/data/points/2025/05.data.ts
@@ -38,4 +38,116 @@ export const pointsData2025_05: IPointEntities = {
       },
     },
   },
+
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0202. Wobbuffet
+  'd77cbd52-f84c-4a7c-bb62-a54c267fe920': {
+    data: {
+      id: 'd77cbd52-f84c-4a7c-bb62-a54c267fe920',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-02',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7c8faea8-5ba1-4c3c-9867-f8f3d9defcd7',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Tone (@Tone)
+  //  0204. Pineco
+  'ff34d1ac-2800-4335-8614-ec82f74a56a9': {
+    data: {
+      id: 'ff34d1ac-2800-4335-8614-ec82f74a56a9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-03',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da602af9-4a1d-40bf-9042-f943f7241a9d',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Padukes (@Padukes)
+  //  0208. Steelix
+  '10ba2d0b-dd76-4343-a38b-32662b5f609f': {
+    data: {
+      id: '10ba2d0b-dd76-4343-a38b-32662b5f609f',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-03',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '09476a48-454e-4a36-88a8-192abacbcf7c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
