5646ced7925bcfba934a6c00247647f3dd06cdc8
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2025-03-12 09:41:23 +0000

    Deploy

--- src/data/points/2025/05.data.ts
+++ src/data/points/2025/05.data.ts
@@ -149,4 +149,560 @@ export const pointsData2025_05: IPointEntities = {
       },
     },
   },
+
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0133. Eevee
+  'b035855e-9f03-4205-b259-23fbcbf49b72': {
+    data: {
+      id: 'b035855e-9f03-4205-b259-23fbcbf49b72',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b5ad7979-4d15-453e-b1a0-a4fcf1ba0120',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0213. Shuckle
+  '440ca8cc-efae-4a83-b562-af832cf548f0': {
+    data: {
+      id: '440ca8cc-efae-4a83-b562-af832cf548f0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-07',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a9056e67-acfe-4264-9f78-06098b5e10dd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Devilish Inferno (@Alex)
+  //  0201. Unown
+  '99a2d6d5-3395-4881-84c9-33c5cb503051': {
+    data: {
+      id: '99a2d6d5-3395-4881-84c9-33c5cb503051',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '3be16e09-d7c4-4ea5-a5bc-8fb3366355b6',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'c47787a9-7b74-4f4b-93e9-e0836ef7cd28',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0214. Heracross
+  '80a250aa-4bca-471f-bc8b-f1234c8f2d66': {
+    data: {
+      id: '80a250aa-4bca-471f-bc8b-f1234c8f2d66',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ca1230a9-d936-4410-a015-7fac682cf2cf',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0214. Heracross
+  '7c536ec6-5f78-4d4d-acb6-bf4fe98cac1d': {
+    data: {
+      id: '7c536ec6-5f78-4d4d-acb6-bf4fe98cac1d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ca1230a9-d936-4410-a015-7fac682cf2cf',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0187. Hoppip
+  'f54d0c25-7bbb-48f2-ac42-e3cdc625ba20': {
+    data: {
+      id: 'f54d0c25-7bbb-48f2-ac42-e3cdc625ba20',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-08',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '45458f0f-3118-4b87-a148-bee667717bc2',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Stuart (@stuart)
+  //  0137. Porygon
+  'be52f23b-ede5-441b-ae19-c23c6c188431': {
+    data: {
+      id: 'be52f23b-ede5-441b-ae19-c23c6c188431',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '32db34f7-0c11-401c-8504-d399aa98e376',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0233. Porygon2
+  '2bd810c3-edf0-4beb-a867-cfe013ed3baf': {
+    data: {
+      id: '2bd810c3-edf0-4beb-a867-cfe013ed3baf',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7fa95f76-105b-45bf-9d9a-00f20329bee3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Stuart (@stuart)
+  //  0177. Natu
+  'b00aa57c-a604-470c-addf-ed7dfc518c9f': {
+    data: {
+      id: 'b00aa57c-a604-470c-addf-ed7dfc518c9f',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5cfdd898-80d0-4fa7-b728-30b28f871ca0',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Keagan (@Keagan)
+  //  0234. Stantler
+  'f7670d77-0d3f-40fb-b18b-43a217b800eb': {
+    data: {
+      id: 'f7670d77-0d3f-40fb-b18b-43a217b800eb',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '6931874b-a4ce-4abb-a16c-c6183a1668ab',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Tone (@Tone)
+  //  0133. Eevee
+  'aeef0d8a-1d66-4fb0-b524-43ef4c007918': {
+    data: {
+      id: 'aeef0d8a-1d66-4fb0-b524-43ef4c007918',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b5ad7979-4d15-453e-b1a0-a4fcf1ba0120',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0204. Pineco
+  '33987713-8786-49c0-b9d6-7a8e3b239683': {
+    data: {
+      id: '33987713-8786-49c0-b9d6-7a8e3b239683',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-11',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da602af9-4a1d-40bf-9042-f943f7241a9d',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Lewis Dobie (@Lewthelegend)
+  //  0220. Swinub
+  '0cf91921-32ad-4f48-a298-608fdc7f2ef9': {
+    data: {
+      id: '0cf91921-32ad-4f48-a298-608fdc7f2ef9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-06',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9dfb92df-d030-412c-9833-d5406c3051e3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Lewis Dobie (@Lewthelegend)
+  //  0190. Aipom
+  'ea629e81-8b2d-4535-a2df-13c48de44b1e': {
+    data: {
+      id: 'ea629e81-8b2d-4535-a2df-13c48de44b1e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-06',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b47d4a57-6547-4a1c-a2bc-4e4aca095d46',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  2/3rds Johto (and Tepig) 2 Mar 2025  to 15 Mar 2025
+  //  Lewis Dobie (@Lewthelegend)
+  //  0204. Pineco
+  '3cec10fa-12cb-4fbb-8647-95b2ca146994': {
+    data: {
+      id: '3cec10fa-12cb-4fbb-8647-95b2ca146994',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-06',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'baab4caa-cf5d-4791-bff3-424c8777e8a3',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da602af9-4a1d-40bf-9042-f943f7241a9d',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
