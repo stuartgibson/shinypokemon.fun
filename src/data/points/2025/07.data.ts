cf05cc4078770fa5473393c42c00f977b7056e85
Stuart Gibson <stuart@abovetheinternet.org>
2025-03-30 16:01:29 +0100

    Deploy

--- src/data/points/2025/07.data.ts
+++ src/data/points/2025/07.data.ts
@@ -1,3 +1,152 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2025_07: IPointEntities = {};
+export const pointsData2025_07: IPointEntities = {
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Tone (@Tone)
+  //  0222. Corsola
+  'b3f3b114-8500-4eb7-a185-7f5654b94a79': {
+    data: {
+      id: 'b3f3b114-8500-4eb7-a185-7f5654b94a79',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '4b1f380b-3d86-4add-aad6-87abb6f173c0',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Al McKinlay (@Al)
+  //  0290. Nincada
+  '958f296b-323c-44df-b2e6-05e031beadec': {
+    data: {
+      id: '958f296b-323c-44df-b2e6-05e031beadec',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'ff4976ce-1519-4ad6-a124-eef49565fc58',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'af56cc6b-3a28-4867-a156-2345080173ec',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0290. Nincada
+  '5258c67e-0a1c-42cc-a600-d529c87f06d9': {
+    data: {
+      id: '5258c67e-0a1c-42cc-a600-d529c87f06d9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'af56cc6b-3a28-4867-a156-2345080173ec',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0774. Minior
+  '80f5b36a-0732-4352-a43f-7a8027ca8850': {
+    data: {
+      id: '80f5b36a-0732-4352-a43f-7a8027ca8850',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'e78e09d4-43d3-44bb-9c87-d2ff0e469e2b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
