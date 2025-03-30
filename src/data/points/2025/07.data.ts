da9ea1d61a39b6463c9d2355d074f0f8fcb5e14d
Stuart Gibson <stuart@abovetheinternet.org>
2025-03-30 16:05:27 +0100

    Deploy

--- src/data/points/2025/07.data.ts
+++ src/data/points/2025/07.data.ts
@@ -77,43 +77,6 @@ export const pointsData2025_07: IPointEntities = {
   },
   //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
   //  Brandon Hero of Ice (@Brandon Heroice18)
-  //  0290. Nincada
-  '5258c67e-0a1c-42cc-a600-d529c87f06d9': {
-    data: {
-      id: '5258c67e-0a1c-42cc-a600-d529c87f06d9',
-      type: 'point',
-      attributes: {
-        ball: null,
-        catchDate: '2025-03-30',
-        firstCatch: false,
-        game: null,
-        method: null,
-        oldSystemPoint: false,
-      },
-      relationships: {
-        competition: {
-          data: {
-            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
-            type: 'competition',
-          },
-        },
-        player: {
-          data: {
-            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
-            type: 'player',
-          },
-        },
-        pokemon: {
-          data: {
-            id: 'af56cc6b-3a28-4867-a156-2345080173ec',
-            type: 'pokemon',
-          },
-        },
-      },
-    },
-  },
-  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
-  //  Brandon Hero of Ice (@Brandon Heroice18)
   //  0774. Minior
   '80f5b36a-0732-4352-a43f-7a8027ca8850': {
     data: {
