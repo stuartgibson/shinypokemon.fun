07194223f978a613c263c11e3c20feb83feefeee
Stuart Gibson <stuart@abovetheinternet.org>
2025-03-31 18:58:06 +0100

    Deploy

--- src/data/points/2025/07.data.ts
+++ src/data/points/2025/07.data.ts
@@ -112,4 +112,190 @@ export const pointsData2025_07: IPointEntities = {
       },
     },
   },
+
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Keagan (@Keagan)
+  //  0091. Cloyster
+  '92890ada-4e24-4f27-bd8d-3d79b687bf60': {
+    data: {
+      id: '92890ada-4e24-4f27-bd8d-3d79b687bf60',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'be37c657-0018-4afc-9bab-dde828130189',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Keagan (@Keagan)
+  //  0924. Tandemaus
+  'd84d20a7-a076-474e-a299-52cbed1ef67c': {
+    data: {
+      id: 'd84d20a7-a076-474e-a299-52cbed1ef67c',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f4124e83-411f-4391-84b8-8816a6716f69',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Keagan (@Keagan)
+  //  0983. Kingambit
+  '38a8a36f-9772-4497-a765-3eed22863e9e': {
+    data: {
+      id: '38a8a36f-9772-4497-a765-3eed22863e9e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b57bc54a-4f8f-4c36-82d5-57493b6fe376',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Padukes (@Padukes)
+  //  0524. Roggenrola
+  'a649e20c-6a6a-4297-b657-0ebc9bb0dbf6': {
+    data: {
+      id: 'a649e20c-6a6a-4297-b657-0ebc9bb0dbf6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1ca0b66b-2791-41c2-bf6d-aeabae1cfdb0',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'cc0af826-8ee8-426a-bd0b-45bc81f50a1c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  I Just Think They’re Neat 🥔 30 Mar 2025  to 12 Apr 2025
+  //  Tone (@Tone)
+  //  0056. Mankey
+  '393fd699-c646-4d8b-a250-d16d48d6bb9c': {
+    data: {
+      id: '393fd699-c646-4d8b-a250-d16d48d6bb9c',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-03-31',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '3b5c9708-9b64-4154-928a-a57f9fbf8f8e',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'e07a9ffb-7f01-4691-a8c0-a05aabbedb20',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
