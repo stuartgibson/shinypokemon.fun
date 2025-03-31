58206607d745773ca239bddc35079b4aa97c1253
Stuart Gibson <stuart@abovetheinternet.org>
2025-03-31 19:06:01 +0100

    Deploy

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,34 +1,33 @@
-import { CompetitionGeneratorComponent } from "./admin/competition-generator/competition-generator.component";
-import { GeneratorsComponent } from "./admin/generators/generators.component";
-import { PointsGeneratorComponent } from "./admin/points-generator/points-generator.component";
-import { PokemonListGeneratorComponent } from "./admin/pokemon-list-generator/pokemon-list-generator.component";
-import { AppComponent } from "./app/app.component";
-import { CompetitionListComponent } from "./competitions/competition-list/competition-list.component";
-import { CompetitionNavComponent } from "./competitions/competition-nav/competition-nav.component";
-import { CompetitionPageComponent } from "./competitions/competition-page/competition-page.component";
-import { CompetitionComponent } from "./competitions/competition/competition.component";
-import { CompetitionsComponent } from "./competitions/competitions/competitions.component";
-import { CurrentCompetitionComponent } from "./competitions/current-competition/current-competition.component";
-import { InlineCompetitionComponent } from "./competitions/inline-competition/inline-competition.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-import { NavComponent } from "./nav/nav.component";
-import { AboutComponent } from "./pages/about/about.component";
-import { RulesComponent } from "./pages/rules/rules.component";
-import { InlinePlayerComponent } from "./player/inline-player/inline-player.component";
-import { PlayerPageComponent } from "./player/player-page/player-page.component";
-import { PlayersComponent } from "./player/players/players.component";
-import { PointComponent } from "./point/point.component";
-import { InlinePokemonComponent } from "./pokemon/inline-pokemon/inline-pokemon.component";
-import { PokemonComponent } from "./pokemon/pokemon/pokemon.component";
-import { CurrentYearComponent } from "./years/current-year/current-year.component";
-import { PreviousYearsComponent } from "./years/previous-years/previous-years.component";
-import { YearPageComponent } from "./years/year-page/year-page.component";
-import { BallComponent } from "./_shared/ball/ball.component";
-import { DateRangeComponent } from "./_shared/date-range/date-range.component";
-import { DateComponent } from "./_shared/date/date.component";
-import { GameComponent } from "./_shared/game/game.component";
-import { MethodComponent } from "./_shared/method/method.component";
+import { BallComponent } from './_shared/ball/ball.component';
+import { DateRangeComponent } from './_shared/date-range/date-range.component';
+import { DateComponent } from './_shared/date/date.component';
+import { GameComponent } from './_shared/game/game.component';
+import { MethodComponent } from './_shared/method/method.component';
+import { CompetitionGeneratorComponent } from './admin/competition-generator/competition-generator.component';
+import { GeneratorsComponent } from './admin/generators/generators.component';
+import { PointsGeneratorComponent } from './admin/points-generator/points-generator.component';
+import { PokemonListGeneratorComponent } from './admin/pokemon-list-generator/pokemon-list-generator.component';
+import { AppComponent } from './app/app.component';
+import { CompetitionListComponent } from './competitions/competition-list/competition-list.component';
+import { CompetitionNavComponent } from './competitions/competition-nav/competition-nav.component';
+import { CompetitionPageComponent } from './competitions/competition-page/competition-page.component';
+import { CompetitionComponent } from './competitions/competition/competition.component';
+import { CompetitionsComponent } from './competitions/competitions/competitions.component';
+import { CurrentCompetitionComponent } from './competitions/current-competition/current-competition.component';
+import { InlineCompetitionComponent } from './competitions/inline-competition/inline-competition.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RulesComponent } from './pages/rules/rules.component';
+import { InlinePlayerComponent } from './player/inline-player/inline-player.component';
+import { PlayerPageComponent } from './player/player-page/player-page.component';
+import { PlayersComponent } from './player/players/players.component';
+import { PointComponent } from './point/point.component';
+import { InlinePokemonComponent } from './pokemon/inline-pokemon/inline-pokemon.component';
+import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
+import { CurrentYearComponent } from './years/current-year/current-year.component';
+import { PreviousYearsComponent } from './years/previous-years/previous-years.component';
+import { YearPageComponent } from './years/year-page/year-page.component';
 
 export const components = [
   AboutComponent,
@@ -44,7 +43,6 @@ export const components = [
   CurrentYearComponent,
   DateComponent,
   DateRangeComponent,
-  FooterComponent,
   GameComponent,
   GeneratorsComponent,
   HomeComponent,
@@ -61,5 +59,5 @@ export const components = [
   RulesComponent,
   PlayerPageComponent,
   PlayersComponent,
-  YearPageComponent
-]
+  YearPageComponent,
+];
