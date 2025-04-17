49ccc82866133cd937c0071d2f8a904e09fe643d
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2025-04-17 17:01:11 +0100

    Remove unrequired import

--- scripts/build-prod.ts
+++ scripts/build-prod.ts
@@ -1,22 +1,24 @@
-import { copyFile, copyFileSync } from "fs";
-import { generateApis } from "./generate-api"
-import {execSync } from 'child_process';
+import { execSync } from 'child_process';
+import { copyFileSync } from 'fs';
+import { generateApis } from './generate-api';
 
-console.log("Running Build Script");
+console.log('Running Build Script');
 
 // Create the static JSON files
-console.log("Generating API files");
+console.log('Generating API files');
 generateApis();
 
 // Run the actual build
-console.log("Running build");
-execSync(
-    'ng build --configuration production --subresource-integrity', 
-    {stdio: 'inherit'}
-);
+console.log('Running build');
+execSync('ng build --configuration production --subresource-integrity', {
+  stdio: 'inherit',
+});
 
 // Make sure the 404 also runs angular
-console.log("Copying files");
-copyFileSync('dist/shinypokemon.fun/browser/index.html', 'dist/shinypokemon.fun/browser/404.html');
+console.log('Copying files');
+copyFileSync(
+  'dist/shinypokemon.fun/browser/index.html',
+  'dist/shinypokemon.fun/browser/404.html'
+);
 
-console.log("Build complete");
+console.log('Build complete');
