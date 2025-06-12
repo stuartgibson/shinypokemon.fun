3d2cb1fa271932bc9c462aa7482bfdd474e3cbe0
stuart@abovetheinternet.org <stuart@abovetheinternet.org>
2025-06-12 16:37:32 +0100

    Points

--- src/data/points/2025/12.data.ts
+++ src/data/points/2025/12.data.ts
@@ -153,4 +153,309 @@ export const pointsData2025_12: IPointEntities = {
       },
     },
   },
+
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Christopher (@Christopher)
+  //  0666. Vivillon
+  'd3f0574b-0c66-4b7a-80b5-44524a32b00d': {
+    data: {
+      id: 'd3f0574b-0c66-4b7a-80b5-44524a32b00d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '264f7da7-e6d0-4a18-a216-df7233d13260',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Tone (@Tone)
+  //  0666. Vivillon
+  '55a2810d-b5ff-46d0-8580-84997799f75d': {
+    data: {
+      id: '55a2810d-b5ff-46d0-8580-84997799f75d',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Tone (@Tone)
+  //  0666. Vivillon
+  '7cee14c2-b19c-4387-aff5-e2432a063211': {
+    data: {
+      id: '7cee14c2-b19c-4387-aff5-e2432a063211',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-09',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Tone (@Tone)
+  //  0666. Vivillon
+  'd81b0e1c-2e02-4f2f-8684-bbcd39ba7dee': {
+    data: {
+      id: 'd81b0e1c-2e02-4f2f-8684-bbcd39ba7dee',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Keagan (@Keagan)
+  //  0666. Vivillon
+  'b4fba128-3031-431b-a477-2e663c5f1062': {
+    data: {
+      id: 'b4fba128-3031-431b-a477-2e663c5f1062',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Keagan (@Keagan)
+  //  0665. Spewpa
+  '391a8abe-2369-4552-be7d-d6aebda3f633': {
+    data: {
+      id: '391a8abe-2369-4552-be7d-d6aebda3f633',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f9d462b2-7ddd-4789-8c30-75e2bf7fb457',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Keagan (@Keagan)
+  //  0666. Vivillon
+  'd493883d-f59f-470f-85a6-6b59e2c6c3a5': {
+    data: {
+      id: 'd493883d-f59f-470f-85a6-6b59e2c6c3a5',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-11',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Vivillon and Friends 8 Jun 2025  to 21 Jun 2025
+  //  Christopher (@Christopher)
+  //  0666. Vivillon
+  '17cc5062-14a0-410d-9ea7-a5dc4ee238c8': {
+    data: {
+      id: '17cc5062-14a0-410d-9ea7-a5dc4ee238c8',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-06-11',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'eec3f81b-bd9b-47ed-8567-a14e45153588',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '264f7da7-e6d0-4a18-a216-df7233d13260',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f1c992c4-d94d-42f4-add7-d16864cdea0f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
