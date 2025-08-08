786b17165940d9a7c80c14ac07e200af3c9c2958
stuart-flowlens <stuart@flowlens.com>
2025-08-08 09:48:57 +0100

    Points

--- src/data/points/2025/16.data.ts
+++ src/data/points/2025/16.data.ts
@@ -116,4 +116,119 @@ export const pointsData2025_16: IPointEntities = {
       },
     },
   },
+
+  //  World Champions 3 Aug 2025  to 16 Aug 2025
+  //  Stuart (@stuart)
+  //  0498. Tepig
+  'b5c5201c-3921-4a65-af2e-15ac3fa266b5': {
+    data: {
+      id: 'b5c5201c-3921-4a65-af2e-15ac3fa266b5',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-08-07',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'f8e70838-5963-4269-861e-44da14295d6d',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '6f4622b1-d9c5-41a9-b83b-970743131a7c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World Champions 3 Aug 2025  to 16 Aug 2025
+  //  Keagan (@Keagan)
+  //  0322. Numel
+  'e505b9e8-fe46-412b-96d5-b403e81a1ef6': {
+    data: {
+      id: 'e505b9e8-fe46-412b-96d5-b403e81a1ef6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-08-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'f8e70838-5963-4269-861e-44da14295d6d',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a409c5b6-da3f-4eaf-9963-ec0b59d5bccd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  World Champions 3 Aug 2025  to 16 Aug 2025
+  //  Keagan (@Keagan)
+  //  0323. Camerupt
+  '47a1c40b-7995-4865-8d26-1057700241e0': {
+    data: {
+      id: '47a1c40b-7995-4865-8d26-1057700241e0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-08-05',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'f8e70838-5963-4269-861e-44da14295d6d',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b20ad4e5-9acc-4685-a60c-ae0fad5daf80',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
