f07fa45dc5d073cd082ec12ba6cf21a19ad6085e
stuart-flowlens <stuart@flowlens.com>
2025-10-05 12:08:21 +0100

    Points

--- src/data/points/2025/20.data.ts
+++ src/data/points/2025/20.data.ts
@@ -1,3 +1,498 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2025_20: IPointEntities = {};
+export const pointsData2025_20: IPointEntities = {
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Tone (@Tone)
+  //  0749. Mudbray
+  '75098ffc-1c3c-4172-9163-d42fb1dc7d57': {
+    data: {
+      id: '75098ffc-1c3c-4172-9163-d42fb1dc7d57',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da630920-69c0-4c34-a96e-d39501ff7d1a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Tone (@Tone)
+  //  0749. Mudbray
+  '725f7836-f61f-41d3-b185-185dc3963055': {
+    data: {
+      id: '725f7836-f61f-41d3-b185-185dc3963055',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da630920-69c0-4c34-a96e-d39501ff7d1a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Tone (@Tone)
+  //  0231. Phanpy
+  '3ff28723-ebb9-4715-8632-2d99b92223ef': {
+    data: {
+      id: '3ff28723-ebb9-4715-8632-2d99b92223ef',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5a1de7c3-bcb3-4eb9-8365-e45e404bb023',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Tone (@Tone)
+  //  0231. Phanpy
+  'faacfcef-d524-448f-97a7-4ca6d69f1bf0': {
+    data: {
+      id: 'faacfcef-d524-448f-97a7-4ca6d69f1bf0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5a1de7c3-bcb3-4eb9-8365-e45e404bb023',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Tone (@Tone)
+  //  0234. Stantler
+  'eb86322f-545d-4342-8ce0-74ad2f9de393': {
+    data: {
+      id: 'eb86322f-545d-4342-8ce0-74ad2f9de393',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '6931874b-a4ce-4abb-a16c-c6183a1668ab',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0234. Stantler
+  '23219a80-dcfc-4f56-ac92-63b1e635e1d6': {
+    data: {
+      id: '23219a80-dcfc-4f56-ac92-63b1e635e1d6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '6931874b-a4ce-4abb-a16c-c6183a1668ab',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0749. Mudbray
+  'c2bb5584-e1f0-4307-8f07-4369dd80438e': {
+    data: {
+      id: 'c2bb5584-e1f0-4307-8f07-4369dd80438e',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da630920-69c0-4c34-a96e-d39501ff7d1a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0749. Mudbray
+  'a720bdc6-5e6d-4c42-a359-3833c034116b': {
+    data: {
+      id: 'a720bdc6-5e6d-4c42-a359-3833c034116b',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'da630920-69c0-4c34-a96e-d39501ff7d1a',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0231. Phanpy
+  'd03c055f-9fef-4e09-85a3-832f83168782': {
+    data: {
+      id: 'd03c055f-9fef-4e09-85a3-832f83168782',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5a1de7c3-bcb3-4eb9-8365-e45e404bb023',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0231. Phanpy
+  '02a72466-e56f-4d25-83c2-4d212240c0a6': {
+    data: {
+      id: '02a72466-e56f-4d25-83c2-4d212240c0a6',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-09-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5a1de7c3-bcb3-4eb9-8365-e45e404bb023',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Christopher (@Christopher)
+  //  1007. Koraidon
+  'f1b8e5dc-6d36-40cc-a92a-aed823cb1cb2': {
+    data: {
+      id: 'f1b8e5dc-6d36-40cc-a92a-aed823cb1cb2',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '264f7da7-e6d0-4a18-a216-df7233d13260',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5225adea-9aa5-4d0c-94fd-dcf00ea83999',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Christopher (@Christopher)
+  //  1008. Miraidon
+  '1e469878-b014-4bc7-bcbe-86fd5687c2e0': {
+    data: {
+      id: '1e469878-b014-4bc7-bcbe-86fd5687c2e0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '264f7da7-e6d0-4a18-a216-df7233d13260',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '8c839b31-a8b3-4612-8cb3-bea16316cd6c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Transport Pokémon 28 Sep 2025  to 11 Oct 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  1008. Miraidon
+  'c79269c3-791a-4fb9-b27f-70ad9a730d0a': {
+    data: {
+      id: 'c79269c3-791a-4fb9-b27f-70ad9a730d0a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '55965841-cb3e-4fe0-a349-5263937512b4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '8c839b31-a8b3-4612-8cb3-bea16316cd6c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
