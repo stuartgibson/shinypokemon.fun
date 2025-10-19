82f989bb15684866b3562762591d88ba3161bc51
stuart-flowlens <stuart@flowlens.com>
2025-10-19 17:41:41 +0100

    Points

--- src/data/points/2025/21.data.ts
+++ src/data/points/2025/21.data.ts
@@ -39,4 +39,157 @@ export const pointsData2025_21: IPointEntities = {
       },
     },
   },
+
+  //  Catch Fliers, Not Feelings. 12 Oct 2025  to 25 Oct 2025
+  //  Keagan (@Keagan)
+  //  0993. Iron Jugulis
+  '8faf63fe-b996-44cc-b6e7-9f46b2093dc0': {
+    data: {
+      id: '8faf63fe-b996-44cc-b6e7-9f46b2093dc0',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-12',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '002a91b3-a2f3-465a-a2f6-062b4539583c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '4aeca7cb-0977-46ae-8269-282e1783eae1',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Catch Fliers, Not Feelings. 12 Oct 2025  to 25 Oct 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0277. Swellow
+  '54ce725e-428f-44c8-ae78-23ca51b1a372': {
+    data: {
+      id: '54ce725e-428f-44c8-ae78-23ca51b1a372',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-13',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '002a91b3-a2f3-465a-a2f6-062b4539583c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9adfc810-90e2-47c0-af33-e11897395efd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Catch Fliers, Not Feelings. 12 Oct 2025  to 25 Oct 2025
+  //  Tone (@Tone)
+  //  0661. Fletchling
+  'a71d410d-b0da-4737-a335-11ab5105fd45': {
+    data: {
+      id: 'a71d410d-b0da-4737-a335-11ab5105fd45',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '002a91b3-a2f3-465a-a2f6-062b4539583c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '4cf0a1fb-d7f6-4a71-8d99-f711ab637626',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Catch Fliers, Not Feelings. 12 Oct 2025  to 25 Oct 2025
+  //  Keagan (@Keagan)
+  //  0016. Pidgey
+  '06ff1962-1526-4914-adc5-a21dbb196629': {
+    data: {
+      id: '06ff1962-1526-4914-adc5-a21dbb196629',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-19',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '002a91b3-a2f3-465a-a2f6-062b4539583c',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '9d2eb67b-06bb-41a3-936c-654a2fa1e475',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
