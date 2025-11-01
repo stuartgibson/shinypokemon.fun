4995348434f342555cd0aa99630e6c8dd13ac0c4
stuart-flowlens <stuart@flowlens.com>
2025-11-01 20:43:47 +0000

    Points:

--- src/data/points/2025/22.data.ts
+++ src/data/points/2025/22.data.ts
@@ -1,3 +1,308 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2025_22: IPointEntities = {};
+export const pointsData2025_22: IPointEntities = {
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Keagan (@Keagan)
+  //  0986. Brute Bonnet
+  '16d8e4ab-e0b7-4388-b597-acbbed7958f3': {
+    data: {
+      id: '16d8e4ab-e0b7-4388-b597-acbbed7958f3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-26',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '049b64b0-42a8-47b0-983b-a0bddbac6e03',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0302. Sableye
+  'f0e50fda-c907-4ed7-8066-286039ff43cf': {
+    data: {
+      id: 'f0e50fda-c907-4ed7-8066-286039ff43cf',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '660c3b60-e28f-4639-be49-39889334c531',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Stuart (@stuart)
+  //  0425. Drifloon
+  '2ced1b34-d2d5-476e-9610-f9faf9dbc1a9': {
+    data: {
+      id: '2ced1b34-d2d5-476e-9610-f9faf9dbc1a9',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '279b1e2e-4145-4829-9f39-d5e8d2e25a31',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Keagan (@Keagan)
+  //  0722. Rowlet
+  '5adcccf4-401d-4643-a0c3-210fb6793ec7': {
+    data: {
+      id: '5adcccf4-401d-4643-a0c3-210fb6793ec7',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-27',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1cf8acbd-c839-4319-9be7-3b7e8659bdf2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'b5e001d3-94f1-4b6f-b7b7-d7a9ebb20ee1',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Christopher (@Christopher)
+  //  0629. Vullaby
+  '28e5a94f-cad2-402a-aa62-6d31047577d4': {
+    data: {
+      id: '28e5a94f-cad2-402a-aa62-6d31047577d4',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-27',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '264f7da7-e6d0-4a18-a216-df7233d13260',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a646d513-ed8b-415b-b371-7653681bebbd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0353. Shuppet
+  '3baee163-d0db-46c0-ac59-ebd9b8186d2b': {
+    data: {
+      id: '3baee163-d0db-46c0-ac59-ebd9b8186d2b',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-27',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '01045994-15ba-4f43-9b6c-abfed6a0d7f4',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Tone (@Tone)
+  //  0710. Pumpkaboo
+  '47a2924d-4a34-4b8c-9054-1dc15e467261': {
+    data: {
+      id: '47a2924d-4a34-4b8c-9054-1dc15e467261',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '92f1c2b5-7a27-41e3-98f5-ae7f8ca98d2b',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Spooky Season 26 Oct 2025  to 8 Nov 2025
+  //  Lewis Dobie (@Lewthelegend)
+  //  0854. Sinistea
+  '26affef7-d4b9-4aa9-bf34-c2c8e39ef5d4': {
+    data: {
+      id: '26affef7-d4b9-4aa9-bf34-c2c8e39ef5d4',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-10-30',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'b538dca6-57f3-4cd5-90bf-3a465542df76',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '68847ddd-c418-47b7-ba3a-404f1db5caf2',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
