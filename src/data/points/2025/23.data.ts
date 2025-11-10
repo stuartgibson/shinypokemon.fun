26becdfb7a559ea17451278bc0f0a294b15423a9
stuart-flowlens <stuart@flowlens.com>
2025-11-10 19:10:35 +0000

    Points

--- src/data/points/2025/23.data.ts
+++ src/data/points/2025/23.data.ts
@@ -1,3 +1,80 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2025_23: IPointEntities = {};
+export const pointsData2025_23: IPointEntities = {
+  //  Pokémon With Jobs 9 Nov 2025  to 22 Nov 2025
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0511. Pansage
+  '5f10744a-9d7b-4319-b264-c28673b9efd3': {
+    data: {
+      id: '5f10744a-9d7b-4319-b264-c28673b9efd3',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-11-10',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'bc2827b2-72ef-49e6-b30d-676762e0a6e7',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'ab32e387-5435-4bc4-86d5-462a8867f1bf',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon With Jobs 9 Nov 2025  to 22 Nov 2025
+  //  Morgan (Leaf) (@Leaf)
+  //  0306. Aggron
+  '07cb5fb2-8468-41ae-8fd8-d7250f6b16a5': {
+    data: {
+      id: '07cb5fb2-8468-41ae-8fd8-d7250f6b16a5',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2025-11-10',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: 'bc2827b2-72ef-49e6-b30d-676762e0a6e7',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'fa7f7600-5379-4166-8e3f-5ccfe683bd91',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '5d6f6deb-a523-431b-9197-2cae11916995',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
