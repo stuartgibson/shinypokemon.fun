291a13bf4ae48b837466b5b6f34cc800c6a4fa17
stuart-flowlens <stuart@flowlens.com>
2026-01-04 21:54:07 +0000

    Points

--- src/data/points/2026/01.data.ts
+++ src/data/points/2026/01.data.ts
@@ -1,3 +1,80 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2026_01: IPointEntities = {};
+export const pointsData2026_01: IPointEntities = {
+  //  Green Shinies 4 Jan 2026  to 17 Jan 2026
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0042. Golbat
+  'dbcec304-004f-4d4a-9c3c-f1fb29750b10': {
+    data: {
+      id: 'dbcec304-004f-4d4a-9c3c-f1fb29750b10',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-04',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '44f736fe-83b3-41e2-ba4e-9c8c68bba181',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'c61e4369-1e15-4bfe-a519-a856e5c69cf1',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Green Shinies 4 Jan 2026  to 17 Jan 2026
+  //  Tone (@Tone)
+  //  0520. Tranquill
+  '9c5d6975-f399-401f-aaff-ee69c3b1c720': {
+    data: {
+      id: '9c5d6975-f399-401f-aaff-ee69c3b1c720',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-04',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '44f736fe-83b3-41e2-ba4e-9c8c68bba181',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'f7feb66d-f5bd-46fe-ad32-50b3dcd799a9',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
