71420eebd4ffaeeeabcafa0b1d2f8aa46df88384
stuart-flowlens <stuart@flowlens.com>
2026-01-19 09:28:28 +0000

    Points

--- src/data/points/2026/02.data.ts
+++ src/data/points/2026/02.data.ts
@@ -1,3 +1,194 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2026_02: IPointEntities = {};
+export const pointsData2026_02: IPointEntities = {
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Stuart (@stuart)
+  //  0810. Grookey
+  '4f2342a9-25a8-433b-8158-b695cab62279': {
+    data: {
+      id: '4f2342a9-25a8-433b-8158-b695cab62279',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '80cfecae-ef2e-437b-bb94-f0309ee3b3d2',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3ca532e9-3cb5-44f4-b157-08ce4b3bf578',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Tone (@Tone)
+  //  0810. Grookey
+  '71bb55f2-ccbc-401d-941e-a6f4d708194f': {
+    data: {
+      id: '71bb55f2-ccbc-401d-941e-a6f4d708194f',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3ca532e9-3cb5-44f4-b157-08ce4b3bf578',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Deathrite (@Deathrite)
+  //  0810. Grookey
+  'da709337-affa-4225-8c5a-7ee4876fd489': {
+    data: {
+      id: 'da709337-affa-4225-8c5a-7ee4876fd489',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '2123e057-079c-42d2-b9d6-a15dfef99a51',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3ca532e9-3cb5-44f4-b157-08ce4b3bf578',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Eden (@FlusherBore)
+  //  0810. Grookey
+  'f4fc3eab-cde3-46a0-8c75-7d22724df307': {
+    data: {
+      id: 'f4fc3eab-cde3-46a0-8c75-7d22724df307',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '85ea4741-ecd3-4493-a24d-88aaf8cd2253',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3ca532e9-3cb5-44f4-b157-08ce4b3bf578',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Jacob K (CarChaser) (@Jacob K)
+  //  0812. Rillaboom
+  '1a37df0f-22c1-4ffb-9571-9f7addde1c43': {
+    data: {
+      id: '1a37df0f-22c1-4ffb-9571-9f7addde1c43',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '82313de1-b54b-44b5-831d-0c66e3522af1',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '81f20cbc-e206-46cb-8114-65e6a5637fbb',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
