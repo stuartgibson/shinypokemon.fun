f5997518f3b88286089820be6e68e355e39c411b
stuart-flowlens <stuart@flowlens.com>
2026-01-31 17:15:04 +0000

    Points

--- src/data/points/2026/02.data.ts
+++ src/data/points/2026/02.data.ts
@@ -191,4 +191,385 @@ export const pointsData2026_02: IPointEntities = {
       },
     },
   },
+
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Tone (@Tone)
+  //  0056. Mankey
+  '3eadcc35-2a05-4f4e-bdda-e5fe99072686': {
+    data: {
+      id: '3eadcc35-2a05-4f4e-bdda-e5fe99072686',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'e07a9ffb-7f01-4691-a8c0-a05aabbedb20',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0056. Mankey
+  '3e38abc3-453a-456c-8c92-f0d54e04ab52': {
+    data: {
+      id: '3e38abc3-453a-456c-8c92-f0d54e04ab52',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'e07a9ffb-7f01-4691-a8c0-a05aabbedb20',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0057. Primeape
+  '2db3b3a9-5eae-40b6-8756-2d9da70ef176': {
+    data: {
+      id: '2db3b3a9-5eae-40b6-8756-2d9da70ef176',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '8b96d379-f31b-4c69-84f0-66f6df3e397f',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0979. Annihilape
+  '08fdcbfa-5f8c-4746-92f2-c4cf713c3d05': {
+    data: {
+      id: '08fdcbfa-5f8c-4746-92f2-c4cf713c3d05',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-24',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '8833b9a4-399e-4a43-a864-3e1b0393fd01',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Lewis Dobie (@Lewthelegend)
+  //  0810. Grookey
+  'e560690e-af2b-402c-ba02-e8c737deafaf': {
+    data: {
+      id: 'e560690e-af2b-402c-ba02-e8c737deafaf',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-18',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '7d054896-a0ea-4368-bff1-856b6abf8419',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3ca532e9-3cb5-44f4-b157-08ce4b3bf578',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0554. Darumaka (Galarian)
+  'eb6f3688-d23e-4d25-a875-2de747cdcb3a': {
+    data: {
+      id: 'eb6f3688-d23e-4d25-a875-2de747cdcb3a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '0f9d997d-62b3-49a7-b5c4-87cc9e2690aa',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0555. Darmanitan (Galarian)
+  '613a1d82-d40f-4800-9c25-947674fca4ec': {
+    data: {
+      id: '613a1d82-d40f-4800-9c25-947674fca4ec',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-26',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '089e11a9-9131-4ae7-9492-df34ddffaa11',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0554. Darumaka
+  'b1ee3726-9ff1-489b-bb71-2dbc7ee8ba28': {
+    data: {
+      id: 'b1ee3726-9ff1-489b-bb71-2dbc7ee8ba28',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-27',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '1bbbb3da-d90e-4cbe-83f1-807358a6d35c',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0555. Darmanitan
+  '494be97c-5264-4cac-a57a-18d2b207715a': {
+    data: {
+      id: '494be97c-5264-4cac-a57a-18d2b207715a',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-27',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '7b16a8cc-41ec-41d5-b04e-69e77765777e',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  All out Bananas! 18 Jan 2026  to 31 Jan 2026
+  //  Philip Starns (@Philip Starns)
+  //  0287. Slakoth
+  '4de78adb-ef8b-450f-bf70-4e041f96f9b2': {
+    data: {
+      id: '4de78adb-ef8b-450f-bf70-4e041f96f9b2',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-01-28',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '38511f37-e4dd-4f26-b075-669d7df189ba',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: 'e7e5548f-6d09-421f-8f37-966949fe41b9',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '41c73fdf-60f6-4139-b418-b226f76199e4',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
 };
