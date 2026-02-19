cadea0e9469bc818f2132f094de7ab41235621c4
stuartgibson <stuart@abovetheinternet.org>
2026-02-19 08:49:36 +0000

    Points

--- src/data/points/2026/04.data.ts
+++ src/data/points/2026/04.data.ts
@@ -1,3 +1,118 @@
 import { IPointEntities } from 'store/reducers';
 
-export const pointsData2026_04: IPointEntities = {};
+export const pointsData2026_04: IPointEntities = {
+  //  Pokémon 30‘s 15 Feb 2026  to 7 Mar 2026
+  //  Tone (@Tone)
+  //  0629. Vullaby
+  'a1f2907d-0524-4e5f-b83f-455b0fcc5c60': {
+    data: {
+      id: 'a1f2907d-0524-4e5f-b83f-455b0fcc5c60',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-02-15',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '50cdc4fb-4be4-4b6e-93b5-dd497b1f47c4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '6b7a4cdb-fd7b-448c-9f03-2b49b4ab3b9d',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: 'a646d513-ed8b-415b-b371-7653681bebbd',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon 30‘s 15 Feb 2026  to 7 Mar 2026
+  //  Brandon Hero of Ice (@Brandon Heroice18)
+  //  0660. Diggersby
+  '52810181-a95e-4f64-a162-39aa332ecd3f': {
+    data: {
+      id: '52810181-a95e-4f64-a162-39aa332ecd3f',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-02-15',
+        firstCatch: true,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '50cdc4fb-4be4-4b6e-93b5-dd497b1f47c4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '1e665730-88cd-4ca3-b03c-961c3a71e749',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '4a78a20a-2482-4147-8a05-941b29a54914',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+  //  Pokémon 30‘s 15 Feb 2026  to 7 Mar 2026
+  //  Deathrite (@Deathrite)
+  //  0120. Staryu
+  '15fd759d-7d96-4724-812e-b1f12d3e9756': {
+    data: {
+      id: '15fd759d-7d96-4724-812e-b1f12d3e9756',
+      type: 'point',
+      attributes: {
+        ball: null,
+        catchDate: '2026-02-17',
+        firstCatch: false,
+        game: null,
+        method: null,
+        oldSystemPoint: false,
+        value: 1,
+      },
+      relationships: {
+        competition: {
+          data: {
+            id: '50cdc4fb-4be4-4b6e-93b5-dd497b1f47c4',
+            type: 'competition',
+          },
+        },
+        player: {
+          data: {
+            id: '2123e057-079c-42d2-b9d6-a15dfef99a51',
+            type: 'player',
+          },
+        },
+        pokemon: {
+          data: {
+            id: '3caf5287-84ef-476a-9b3e-9e8a741fe4d3',
+            type: 'pokemon',
+          },
+        },
+      },
+    },
+  },
+};
